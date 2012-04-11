16236c4d08b31994b3f2716babdd9857d71074f5
Nikita Malyavin <nikitamalyavin@gmail.com>
2012-04-11 23:09:20 +1100

    asm: late polishing

--- asm/cache_effect.c
+++ asm/cache_effect.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef long long LL;
+typedef unsigned long long LL;
 
-inline LL cpuid() __attribute__ ((always_inline)); //только хардкор
-LL cpuid(){
-     unsigned a, d;
+inline LL rdtsc() __attribute__ ((always_inline)); //только хардкор
+LL rdtsc(){
+     LL a, d;
      asm volatile("rdtsc" : "=a" (a), "=d" (d));
-
-     return (((LL)a) | (((LL)d) << 32));
+     return a | (d << 32);
 }
 
 int main(){
     int sz = 128;
     for(int i = 0; i < 22; i++, sz <<= 1){
-        char *c = malloc(sz * sizeof(char));
+        char *c = malloc(sz);
         char *d = malloc(sz);
-        LL s = cpuid();
+        LL s = rdtsc();
         memcpy(c, d, sz);
-//        for(int i = 0; i < sz; i++){
-//            d[i] = c[i];
+//        for(int j = 0; j < sz; j++){
+//            d[j] = c[j];
 //        }
-        LL e = cpuid() - s;
-        printf("sz=%d,\ttime %lld sec;\t%.2f on byte\n", sz, e, ((double)e) / sz);
+        LL e = rdtsc() - s;
+        printf("sz=%d,\ttime %lld;\t%.2f per byte\n", sz, e, ((double)e) / sz);
         free(d);
-//        free(c);
+        free(c);
     }
 }
